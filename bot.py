59b4f6969a8650851eff6a34fb16197092ece853
roystekii2 <52863421+roystekii2@users.noreply.github.com>
2019-07-14 02:36:10 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,13 +1,24 @@
-import discord
-from discord.ext import commands 
-client = commands.Bot(command_prefix = "=")
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import logging
+import os
 
-@client.event
+from discord.ext import commands
+
+# you need this line, and you forgot it
+logging.basicConfig(level='INFO')
+
+
+client = commands.Bot(command_prefix="=")
+
+
+# do NOT use @client.event here, it breaks everything
+@client.listen() 
 async def on_message(message):
-    nsfw_ch = client.get_channel(593050161467621395)
-    if message.channel == nsfw_ch:
-        if not message.attachments:
-            message.delete()
+    # put your channel ID here
+    nsfw_channel = client.get_channel(392215236612194305) 
+    if message.channel == nsfw_channel and not message.attachments:
+        await message.delete()
 
 
 client.run(os.environ['TOKEN'])

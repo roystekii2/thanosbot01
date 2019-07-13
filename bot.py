6f17aaf3d80591422d76629889fb747e83d7d6dd
roystekii2 <52863421+roystekii2@users.noreply.github.com>
2019-07-14 02:39:31 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
+# -- coding: utf-8 --
 import logging
 import os
 
@@ -15,8 +15,7 @@ client = commands.Bot(command_prefix="=")
 # do NOT use @client.event here, it breaks everything
 @client.listen() 
 async def on_message(message):
-    593050161467621395
-    nsfw_channel = client.get_channel(392215236612194305) 
+    nsfw_channel = client.get_channel(593050161467621395) 
     if message.channel == nsfw_channel and not message.attachments:
         await message.delete()
 

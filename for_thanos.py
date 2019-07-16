7639f5e9af83eb81f3c9ce3be9f9b22c203505ed
roystekii2 <52863421+roystekii2@users.noreply.github.com>
2019-07-16 20:37:09 +0900

    Update for_thanos.py

--- for_thanos.py
+++ for_thanos.py
@@ -16,7 +16,10 @@ client = commands.Bot(command_prefix="=")
 @client.listen() 
 async def on_message(message):
     nsfw_channel = client.get_channel(593050161467621395) 
+    general = client.get_channel(586147813730025503) 
     if message.channel == nsfw_channel and not message.attachments:
+        await message.delete()
+    elif message.channel == general and message.attachments:
         await message.delete()
 
 

aa659588005f64926dba4778f5ff25cb4b13cd99
roystekii2 <52863421+roystekii2@users.noreply.github.com>
2019-07-30 23:04:44 +0900

    Update for_thanos.py

--- for_thanos.py
+++ for_thanos.py
@@ -8,19 +8,16 @@ from discord.ext import commands
 # you need this line, and you forgot it
 logging.basicConfig(level='INFO')
 
-
-client = commands.Bot(command_prefix="=")
-
-
-# do NOT use @client.event here, it breaks everything
-@client.listen() 
-async def on_message(message):
-    nsfw_channel = client.get_channel(593050161467621395) 
-    general = client.get_channel(586147813730025503) 
-    if message.channel == nsfw_channel and not message.attachments:
-        await message.delete()
-    elif message.channel == general and message.attachments:
-        await message.delete()
-
-
-client.run(os.environ['TOKEN'])
+import discord
+from discord.ext import commands
+client = commands.Bot(command_prefix = "?")
+@client.command(aliases = ["amigo"])
+async def Amigo(ctx):
+    await ctx.send(f"I Love You 3000, {ctx.message.author.mention}")
+@client.command()
+async def breed(ctx):
+    await ctx.send("I was a Miniature Pinscher!")
+@client.command()
+async def fruit(ctx):
+    await ctx.send("My favorite fruit is Apple!")
+client.run("NjA1MzQ0Njk2MDA4NDQxODU3.XUBLTQ.3Ex_nVLLH4zcKRAK3bc_09Q9ps0")

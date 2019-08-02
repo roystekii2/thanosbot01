88b16a3b2df79aa58b4b1659579f0ee7193aad80
roystekii2 <52863421+roystekii2@users.noreply.github.com>
2019-08-02 18:48:50 +0900

    Update for_thanos.py

--- for_thanos.py
+++ for_thanos.py
@@ -2,13 +2,13 @@
 # -- coding: utf-8 --
 import logging
 import os
-
 from discord.ext import commands
+from discord.utils import get
 
 # you need this line, and you forgot it
 logging.basicConfig(level='INFO')
 
-client = commands.Bot(command_prefix="=")
+client = commands.Bot(command_prefix="=", case_insensitive=True)
 
 # do NOT use @client.event here, it breaks everything
 @client.listen() 
@@ -19,5 +19,16 @@ async def on_message(message):
         await message.delete()
     elif message.channel == general and message.attachments:
         await message.delete()
+
+#ngl I've never used a command system like this before so I'm kind of going improv
+
+@client.command(pass_context=True) # this makes sure the ctx var is there 
+@commands.has_role("Admin")
+asnyc def mute(ctx, user: discord.Member):
+	client.add_roles(user, get(ctx.guild, "Muted")) # change "Muted"
+  await ctx.send('{0} has been muted!'.format(user))
+  # I think this should work but honestly I have no idea
+	
+  
 
 client.run(os.environ['TOKEN'])

f4febc684c5fa6023896950adf38fcbfccd4c740
roystekii2 <52863421+roystekii2@users.noreply.github.com>
2019-08-02 22:31:30 +0900

    Update for_thanos.py

--- for_thanos.py
+++ for_thanos.py
@@ -47,6 +47,16 @@ async def jointheforce(ctx):
     embed.set_image(url ="https://static.comicvine.com/uploads/original/11113/111131285/6475508-9502292318-64162.gif ")
     await ctx.send("You are now my child...", embed = embed)
     await ctx.message.delete()
+	
+	
+@client.command()
+async def leavetheteam(ctx):
+    user = ctx.message.author 
+    await user.remove_roles(discord.utils.get(ctx.guild.roles, name = "Thanos' Children"))
+    embed = discord.Embed(title = "Farewell {0} to the family... You are on your own now...".format(ctx.message.author.name))
+    embed.set_image(url ="https://i1.wp.com/78.media.tumblr.com/b4171532756933ce3014e9ddffe0fbce/tumblr_pcp1aiorRo1sc0ffqo5_540.gif?w=605&ssl=1 ")
+    await ctx.send("You have failed me... {ctx.message.author.mention} left the family...", embed = embed)
+    await ctx.message.delete()
 
 
 client.run(os.environ['TOKEN'])

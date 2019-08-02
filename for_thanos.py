4031e4d0406aa4e055fdb60b30fdc260d0a6ca7a
roystekii2 <52863421+roystekii2@users.noreply.github.com>
2019-08-02 21:28:36 +0900

    Update for_thanos.py

--- for_thanos.py
+++ for_thanos.py
@@ -42,11 +42,11 @@ async def unmute(ctx, user: discord.Member):
 async def jointheforce(ctx):
   await user.add_roles(discord.utils.get(ctx.guild.roles, name = "Thanos' Children"))
   if 5 == 5:
-    with open("Image.webp", "wb") as f:
+  with open("Image.webp", "wb") as f:
         f.write(requests.get("https://static.comicvine.com/uploads/original/11113/111131285/6475508-9502292318-64162.gif").content)
-    fi = discord.File("Image.webp")
-    await ctx.send("You are now my child...", file = fi)
-    await ctx.message.delete()
+  fi = discord.File("Image.webp")
+  await ctx.send("You are now my child...", file = fi)
+  await ctx.message.delete()
 
 
 client.run(os.environ['TOKEN'])

6af0255faf2c4bc207bd72d8d829af6cbec2feb0
roystekii2 <52863421+roystekii2@users.noreply.github.com>
2019-11-11 22:33:01 +0000

    Update for_thanos.py

--- for_thanos.py
+++ for_thanos.py
@@ -33,8 +33,8 @@ async def on_message(message):
 @client.command
 @commands.has_any_role("Admin", "Mod", "Chat Mod")
 async def ban(ctx, member : discord.member, * , reason=None): 
- await member.ban(reason=reason)
- await ctx.send('{0} was snapped out of existence!'.format(user))
+    await member.ban(reason=reason)
+    await ctx.send('{0} was snapped out of existence!'.format(user))
     await ctx.message.delete()
 
 @client.command() 
